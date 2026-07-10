a05e05d6935241af9d7d3467edab2d804776e56d
THybrid21 <124342283+THybrid21@users.noreply.github.com>
2026-07-11 04:25:44 +0930

    Revert "Merge remote-tracking branch 'upstream/development' into Clangen-Update"
    This reverts commit 4957a81f9b8fafab69abbde9c02b3421f502e331, reversing
    changes made to febc92e247747cabb9358ab85f20eecb5572fd7e.

--- scripts/events.py
+++ scripts/events.py
@@ -2376,10 +2376,8 @@ def handle_illnesses_or_illness_deaths(cat):
     #                           decide if cat dies                                 #
     # ---------------------------------------------------------------------------- #
     # if triggered_death is True then the cat will die
+    triggered_death = False
     triggered_death = Condition_Events.handle_illnesses(cat, game.clan.current_season)
-    if not triggered_death:
-        handle_outbreaks(cat)
-
     return triggered_death
 
 
@@ -2400,18 +2398,16 @@ def handle_outbreaks(cat):
     already_sick_count = len(already_sick)
 
     # round up the living kitties
-    healthy_cats = list(
+    alive_cats = list(
         filter(
-            lambda kitty: kitty.status.alive_in_player_clan and not kitty.is_ill(),
+            lambda kitty: (kitty.status.alive_in_player_clan and not kitty.is_ill()),
             Cat.all_cats.values(),
         )
     )
-    healthy_count = len(healthy_cats)
+    alive_count = len(alive_cats)
 
     # if large amount of the population is already sick, stop spreading
-    if already_sick_count >= healthy_count * get_config(
-        "condition_related.illness_percentage_max"
-    ):
+    if already_sick_count >= alive_count * 0.25:
         return
 
     meds = find_alive_cats_with_rank(
@@ -2426,12 +2422,11 @@ def handle_outbreaks(cat):
         if cat.illnesses[illness]["infectiousness"] == 0:
             continue
         chance = cat.illnesses[illness]["infectiousness"]
-        chance += len(meds) * get_config("condition_related.med_infection_reduction")
+        chance += len(meds) * 7
         if not int(random.random() * chance):  # 1/chance to infect
             # fleas are the only condition allowed to spread outside of cold seasons
             if (
-                game.clan.current_season
-                not in get_config("condition_related.illness_outbreak_season")
+                game.clan.current_season not in ["Leaf-bare", "Leaf-fall"]
                 and illness != "fleas"
             ):
                 continue
@@ -2445,23 +2440,22 @@ def handle_outbreaks(cat):
 
             if illness == "kittencough":
                 # adjust alive cats list to only include kittens
-                healthy_cats = list(
+                alive_cats = list(
                     filter(
                         lambda kitty: (
                             kitty.status.rank.is_baby()
                             and kitty.status.alive_in_player_clan
-                            and not kitty.is_ill()
                         ),
                         Cat.all_cats.values(),
                     )
                 )
-                healthy_count = len(healthy_cats)
+                alive_count = len(alive_cats)
 
-            max_infected = int(healthy_count / 2)  # 1/2 of alive cats
+            max_infected = int(alive_count / 2)  # 1/2 of alive cats
             # If there are less than two cat to infect,
             # you are allowed to infect all the cats
             if max_infected < 2:
-                max_infected = healthy_count
+                max_infected = alive_count
             # If, event with all the cats, there is less
             # than two cats to infect, cancel outbreak.
             if max_infected < 2:
@@ -2479,7 +2473,7 @@ def handle_outbreaks(cat):
 
             infected_names = []
             involved_cats = []
-            infected_cats = random.sample(healthy_cats, infected_count)
+            infected_cats = random.sample(alive_cats, infected_count)
             for sick_meowmeow in infected_cats:
                 infected_names.append(str(sick_meowmeow.name))
                 involved_cats.append(sick_meowmeow.ID)
